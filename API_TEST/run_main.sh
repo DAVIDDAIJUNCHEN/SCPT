140b4cc1c6a5a68cb41cbc95787d59b3f2a2be2f
Daijun Chen <Admin@MacBook-Pro.local>
2026-08-11 17:44:02 +0800

    move all files into API_TEST

---
+++ API_TEST/run_main.sh
@@ -0,0 +1,36 @@
+#!/user/env/bash 
+
+step=$1
+
+if [ $step == "1" ]; then 
+    echo "step 1: call deepseek api from official mode"
+
+    export DEEPSEEK_ENDPOINT="https://api.deepseek.com"
+    export DEEPSEEK_MODEL="deepseek-chat"
+    export DEEPSEEK_API_KEY="API_KEY"
+    export DEEPSEEK_MODE="official"
+
+    python ./deepseek_v4_bench.py
+fi
+
+if [ $step = "2" ]; then
+    echo "step 2: call deepseek api from local mode (NIM mode)" 
+
+    export DEEPSEEK_ENDPOINT="http://10.32.1.3:30466/v1"
+    export DEEPSEEK_MODEL="deepseek-ai/DeepSeek-V4-Flash"
+    export DEEPSEEK_MODE="local"
+    
+    python ./deepseek_v4_bench.py
+fi
+
+if [ $step = "3" ]; then
+    echo "step 3: call deepseek api from local mode (AI platform mode)"
+    
+    export DEEPSEEK_ENDPOINT="http://10.255.12.38:13206/v1"
+    export DEEPSEEK_MODEL="deepseek-ai/DeepSeek-V4-Flash"
+    export DEEPSEEK_MODE="local"
+    export DEEPSEEK_BEARER_TOKEN="BEAR_TOKEN"
+    
+    python ./deepseek_v4_bench.py
+fi
+
