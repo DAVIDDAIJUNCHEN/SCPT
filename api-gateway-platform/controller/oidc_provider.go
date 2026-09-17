fce39c6f813f8b400fedd03c7a549c47e7f2e612
Daijun Chen <Admin@MacBook-Pro.local>
2026-09-18 00:12:36 +0800

    fix: logger import 路径修正

--- api-gateway-platform/controller/oidc_provider.go
+++ api-gateway-platform/controller/oidc_provider.go
@@ -8,7 +8,7 @@ import (
 	"time"
 
 	"github.com/QuantumNous/new-api/common"
-	"github.com/QuantumNous/new-api/common/logger"
+	"github.com/QuantumNous/new-api/logger"
 	"github.com/QuantumNous/new-api/middleware"
 	"github.com/QuantumNous/new-api/model"
 	"github.com/QuantumNous/new-api/service"
